e31ce0d8162da8c1dd78dd97bc477f855d9a4697
jahofman <Jasper.Hofman@UGent.be>
2019-02-12 17:41:11 +0100

    adding blank classes

--- Looter.java
+++ Looter.java
@@ -1,4 +1,25 @@
 package EdgevillePKLooter;
 
-public class Looter {
+import org.powerbot.script.PollingScript;
+import org.powerbot.script.Script;
+import org.powerbot.script.rt4.ClientContext;
+
+@Script.Manifest(name="EdgevillePKLooter", description = "Loots PK drops and banks at Edgeville", properties = "author=Tortidip; topic=999; client=4;")
+public class Looter  extends PollingScript<ClientContext> {
+
+    @Override
+    public void start(){
+        System.out.println("Starting script");
+    }
+
+    @Override
+    public void stop(){
+        System.out.println("Stopping script");
+    }
+
+    @Override
+    public void poll() {
+        //main loop
+
+    }
 }
---
+++ tasks/Loot.java
@@ -0,0 +1,21 @@
+package EdgevillePKLooter.tasks;
+
+import EdgevillePKLooter.Task;
+import org.powerbot.script.rt4.ClientContext;
+
+public class Loot extends Task {
+
+    public Loot(ClientContext ctx) {
+        super(ctx);
+    }
+
+    @Override
+    public boolean activate() {
+        return false;
+    }
+
+    @Override
+    public void execute() {
+
+    }
+}
